b12756de81430674771168cb9e8293c138c5db52
Kishan Patel <kspatel2018@gmail.com>
2015-08-21 14:54:29 -0400

    customize information

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,3 +1,23 @@
+$("a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top -50
+     }, 700, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
 $(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
 
    // prevent default anchor click behavior
